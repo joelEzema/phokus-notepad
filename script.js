9c42ba90fc3d0a6e7c852f5dd4e51e456e6021ac
Joel Ezema <ezemajoel8@gmail.com>
2026-08-31 17:13:18 +0100

    Added the Distraktion counter feature
    Two features down...

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const statusMsg = document.getElementById("status");
 const notepad = document.getElementById("notepad");
-const distractionCounter = document.getElementById("distraction-counter");
+let distractionCounter = document.getElementById("distraction-counter");
+const resetDistPopup = document.getElementById("reset-dist-popup");
+const resetAgree = document.getElementById("reset-agree");
+const resetRefuse = document.getElementById("reset-refuse");
+
+let distraktions = 0;
 
 const savedText = localStorage.getItem("draft");
+const savedDistraktions = parseInt(localStorage.getItem("distNo"));
 
 if(savedText){
     notepad.value = savedText;
+}
+
+if(savedDistraktions){
+    distraktions = savedDistraktions;
+    distractionCounter.textContent = `Distraktions: ${distraktions}`;
 }
 
 notepad.addEventListener("input", 
@@ -15,7 +26,13 @@ notepad.addEventListener("input",
 );
 
 document.addEventListener("visibilitychange", () => {
+    localStorage.setItem("distNo", distraktions);
+})
+
+document.addEventListener("visibilitychange", () => {
     if(document.hidden){
+        distraktions++;
+        distractionCounter.textContent = `Distraktions: ${distraktions}`;
         statusMsg.textContent = "Distrakted?";
         statusMsg.style.color = "ef4444";
 
@@ -31,4 +48,20 @@ document.addEventListener("visibilitychange", () => {
             }, 2500)
         }, 4000)
     }
+})
+
+distractionCounter.addEventListener("click", () => {
+    resetDistPopup.showModal();
+})
+
+resetAgree.addEventListener("click", () => {
+    distraktions = 0;
+    console.log(distraktions);
+    localStorage.setItem("distNo", distraktions);
+    distractionCounter.textContent = `Distraktions: ${distraktions}`;
+    resetDistPopup.close()
+})
+
+resetRefuse.addEventListener("click", () => {
+    resetDistPopup.close();
 })
